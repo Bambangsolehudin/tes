14472ea11e535061172d948c189cfaa00d5f1cc9
Bambang Solhud <bamsolihin1@gmail.com>
2022-09-23 03:00:04 +0700

    tes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import data from './data.js'
 
 function App() {
+  var temp = [];
+  var replacer = 'Before';
+  var dataCoba = data
+
+  dataCoba.forEach(elmn => {
+    setData(elmn)
+  })
+
+  function setData(data){
+    temp = []
+    data.forEach((el,i) => {
+      if (i != 0) {
+        if (data[i] === data[i - 1]) {
+          temp.push(i);
+        }
+      }
+      if (data[i] === data[i + 1]) {
+        temp.push(i);
+      }
+    });
+    temp.forEach((e,i) => {
+      data[temp[i]] = replacer;
+    });
+    return data;
+  }
+
+  dataCoba.forEach((e,index)=>{
+    e.forEach((el,i) => {
+      e[0] = 'baris'+(index+1)+ ' '
+      e[i] = e[i]+" "
+    })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h4 className="text-center my-4">
+        Tes Coding
+      </h4>
+      {
+        dataCoba.map((e,index) => {
+          return<div className='card p-2 my-2 w-100'>[{e}]</div>
+        })
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
